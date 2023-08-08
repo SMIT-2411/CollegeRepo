0f592eb6ce958bb702de65c551c38652239ad0b7
SMIT-2411 <smit24@icloud.com>
2023-08-08 21:32:15 +0530

    Initial Commit

---
+++ CollegeRepo/CollegeRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeRepoApp.swift
+//  CollegeRepo
+//
+//  Created by Smit Patel on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CollegeRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
