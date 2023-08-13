0bde17f7f997661be2cf17bd2c0173eb67d7e401
SMIT-2411 <smit24@icloud.com>
2023-08-13 13:38:08 +0530

    //changed every path for the new name

--- LNM Quest UITests/LNMQuestUITestsLaunchTests.swift
+++ LNM Quest UITests/LNMQuestUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class CollegeRepoUITestsLaunchTests: XCTestCase {
+final class LNMQuestUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
--- LNMQuest/LNMQuestApp.swift
+++ LNMQuest/LNMQuestApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct CollegeRepoApp: App {
+struct LNMQuestApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
